3d2f0690632f90f35b9eec3d3eed30d37c3b3ef6
silentcoder0407 <63408329+silentcoder0407@users.noreply.github.com>
2020-04-10 13:56:42 +0800

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <br>
 <div align="center">
-<small>Copyright &copy; <?php echo date("Y");?> <a href="about.php">ALREJJ Group of Companies</a></small>
+<small>Copyright &copy; <?php echo date("Y");?> <a href="about.php">ALREJJ Group of Companiess</a></small>
 </div>
 
 
