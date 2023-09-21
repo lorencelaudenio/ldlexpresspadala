6a6fdd557484491e35a05c5ce557973f7e9f6e70
Lorence Laudenio <63408329+lorencelaudenio@users.noreply.github.com>
2023-09-21 16:55:15 +0800

    Fix and update code
    Fixed undefined array error

--- index.php
+++ index.php
@@ -5,13 +5,12 @@ include("nav.php");
 include("global_variables.php");
 include("headers.php");
 
-
 //redirect to login if no variable set for empid
 if(!isset($g_username) || empty($g_username)){
 	header("location: login.php");
 }
 
-$txn_no = $_POST['txn_no'];
+$txn_no = $_POST['txn_no'] ?? null;
 echo '<div class="container p-3 bg-primary text-white">';
 //search
 if(isset($_POST['search'])){
@@ -57,7 +56,7 @@ if(isset($_POST['search'])){
 <h2>Track</h2>
 <form method="POST" action="index.php" class="was-validated">
 <div class="input-group mb-3">
-<input placeholder="Enter Transaction Number" class="form-control" type="text" name="txn_no" value="<?php echo $db_txn_no; ?>" autofocus required>
+<input placeholder="Enter Transaction Number" class="form-control" type="text" name="txn_no" value="<?php echo $db_txn_no ?? null; ?>" autofocus required>
 <div class="input-group-append">
 <input class="btn btn-success mb-2" type="submit" name="search" value="Search">
 </div>
@@ -67,46 +66,46 @@ if(isset($_POST['search'])){
 <div class="input-group-prepend">
   <span class="input-group-text">Status:</span>
 </div>
-<input type="text" class="form-control"  name="status" value="<?php echo ucfirst($db_status); ?>" readonly>
+<input type="text" class="form-control"  name="status" value="<?php echo ucfirst($db_status ?? null); ?>" readonly>
 </div>
 
 	<div class="input-group mb-3">
     <div class="input-group-prepend">
       <span class="input-group-text">Sender:</span>
     </div>
-    <input type="text" class="form-control" name="sender" value="<?php echo $db_sender; ?>"  readonly>
+    <input type="text" class="form-control" name="sender" value="<?php echo $db_sender ?? null; ?>"  readonly>
 	<div class="input-group-prepend">
       <span class="input-group-text">Mobile No.:</span>
     </div>
-    <input type="text" class="form-control" name="sender_cp_no" value="<?php echo $db_sender_cp_no; ?>" readonly>
+    <input type="text" class="form-control" name="sender_cp_no" value="<?php echo $db_sender_cp_no ?? null; ?>" readonly>
   </div>
 	<div class="input-group mb-3">
     <div class="input-group-prepend">
       <span class="input-group-text">Receiver:</span>
     </div>
-    <input type="text" class="form-control" name="receiver" value="<?php echo $db_receiver; ?>"  readonly>
+    <input type="text" class="form-control" name="receiver" value="<?php echo $db_receiver ?? null; ?>"  readonly>
 	<div class="input-group-prepend">
       <span class="input-group-text">Mobile No.:</span>
     </div>
-    <input type="text" class="form-control" name="receiver_cp_no" value="<?php echo $db_receiver_cp_no; ?>" readonly>
+    <input type="text" class="form-control" name="receiver_cp_no" value="<?php echo $db_receiver_cp_no ?? null; ?>" readonly>
   </div>
 
 <div class="container p-3 my-3 border">
 		<div class="form-group">
 		<label for="name">Destination Branch:</label>
-		<input type="text" class="form-control"  name="dest" value="<?php echo ucfirst($db_dest); ?>"  readonly>
+		<input type="text" class="form-control"  name="dest" value="<?php echo ucfirst($db_dest ?? null); ?>"  readonly>
 	</div>
 	<div class="form-group">
 		<label for="name">Amount:</label>
-		<input type="text" class="form-control"  name="amt" value="<?php echo $db_amt; ?>"  readonly>
+		<input type="text" class="form-control"  name="amt" value="<?php echo $db_amt ?? null; ?>"  readonly>
 	</div>
 	<div class="form-group">
 		<label for="name">Purpose of Transaction:</label>
-		<input type="text" class="form-control"  name="purp" value="<?php echo $db_purp; ?>"  readonly>
+		<input type="text" class="form-control"  name="purp" value="<?php echo $db_purp ?? null; ?>"  readonly>
 	</div>
 	<div class="form-group">
 		<label for="name">Relationship to Receiver:</label>
-		<input type="text" class="form-control"  name="relship" value="<?php echo $db_relship; ?>"  readonly>
+		<input type="text" class="form-control"  name="relship" value="<?php echo $db_relship ?? null; ?>"  readonly>
 	</div>
 </div>
 
@@ -115,15 +114,15 @@ if(isset($_POST['search'])){
     <div class="input-group-prepend">
       <span class="input-group-text">Processed by:</span>
     </div>
-    <input type="text" class="form-control" name="processed_by" value="<?php echo $db_processed_by; ?>"  readonly>
-    <input type="text" class="form-control" name="date_time_sent"  value="<?php echo $db_date_time_sent; ?>"  readonly>
+    <input type="text" class="form-control" name="processed_by" value="<?php echo $db_processed_by ?? null; ?>"  readonly>
+    <input type="text" class="form-control" name="date_time_sent"  value="<?php echo $db_date_time_sent ?? null; ?>"  readonly>
   </div>
 	<div class="input-group mb-3">
     <div class="input-group-prepend">
       <span class="input-group-text">Released by:</span>
     </div>
-    <input type="text" class="form-control" name="processed_by" value="<?php echo $db_released_by; ?>"  readonly>
-    <input type="text" class="form-control" name="date_time_sent"  value="<?php echo $db_date_time_claimed; ?>"  readonly>
+    <input type="text" class="form-control" name="processed_by" value="<?php echo $db_released_by ?? null; ?>"  readonly>
+    <input type="text" class="form-control" name="date_time_sent"  value="<?php echo $db_date_time_claimed ?? null; ?>"  readonly>
   </div>
 
 </div>
