fe18baa045314c7806e5acdd513281e8c6452525
Lorence Laudenio <63408329+lorencelaudenio@users.noreply.github.com>
2023-09-21 16:54:11 +0800

    update global variable

--- footer.php
+++ footer.php
@@ -5,7 +5,7 @@ include("headers.php");
 <div class="container">
 <footer class="page-footer font-small blue">
 <div class="row">
-  <div class="col footer-copyright text-left py-3"><small><p>Express your money padala.</small></p></div>
+  <div class="col footer-copyright text-left py-3"><small><p><?php $g_tagline;?></small></p></div>
   <div class="col footer-copyright text-right py-3"><small>Copyright &copy; <?php echo date("Y");?> <a href="about.php"><?php echo $g_receipttitle;?></a></small></div>
   </div>
  </footer>
