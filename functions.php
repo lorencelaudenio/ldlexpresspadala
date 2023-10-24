27e0cbf0c82174d05ba39aba404954739d5091de
Lorence Laudenio <laudeniolorence@gmail.com>
2023-10-24 23:45:35 +0800

    deleted irrelevant files

--- functions.php
+++ functions.php
@@ -1,2 +1,14 @@
+<script>
+    function copyTXN() {
+        var copyText = document.getElementById("txn");
 
+        
+
+/* Copy the text inside the text field */
+document.execCommand("copy");
+
+/* Alert the copied text */
+alert("Copied the text: " + copyText.value);
+}
+</script>
 
